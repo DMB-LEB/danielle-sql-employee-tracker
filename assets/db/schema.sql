9c1ed896fd5fee767879d39d80d45e845231be47
DMB-LEB <nhnative2020@gmail.com>
2023-09-20 13:50:47 -0400

    completed & tested schema.sql

---
+++ assets/db/schema.sql
@@ -0,0 +1,34 @@
+Drop DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department(
+    department_id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    store_number INT,
+    PRIMARY KEY (department_id)
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (role_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    department_id INT,
+    manager_id INT NULL,
+    store_number INT,
+    PRIMARY KEY (employee_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) 
+    );
